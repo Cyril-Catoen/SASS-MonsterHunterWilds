264c6ebdd84f484b3b04c254ef0e8e80f3d37be3
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-03-18 10:25:03 +0100

    Création des dossiers et tronc communs - config.php / architecture mvc dont controller index.php et view index.php / css-sass

---
+++ view/partial/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <!-- Permet de configurer le site en responsive -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+        <!-- Import du fichier style.css -->
+        <link rel="stylesheet" href="../css/style.css?v=<?= time(); ?>" />
+
+        <!-- Nomme l'onglet du navigateur -->
+        <title>Monster Hunter Wilds</title>
+</head>
+
+<body>
+<header>
+</header>
