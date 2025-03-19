a7fdec089aaefbbf20b1ace6a8eb327343930a61
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-03-19 14:48:49 +0100

    Modificaiton du nom de la view partial main-section1.php pour main.php

---
+++ view/partial/main.php
@@ -0,0 +1,78 @@
+<main>
+    <!-- section 1 -->
+    <section> 
+    <div id="chasse">
+        <div>
+            <h2>La chasse est ouverte</h2>
+        </div>
+        <div>
+            <h3>La zone interdite est a portee</h3>
+            <p>Dans Monster Hunter Wilds, les Terres orientales constituent une région riche en ressources rares et en biodiversité. Les chasseurs y explorent des environnements variés, tels que les Plaines venteuses et la Forêt écarlate, à la recherche de matériaux précieux comme le Pollen de fleur de lune, essentiel pour améliorer leur équipement. Ces zones sont également le territoire de monstres redoutables, notamment le Rey Dau, une wyverne volante dominante des Plaines venteuses, et la Lala Barina, une créature arachnide qui rôde dans la Forêt écarlate. Les chasseurs doivent s'adapter aux conditions climatiques dynamiques et aux cycles jour/nuit, qui influencent le comportement des créatures et la disponibilité des ressources. Le village de Kunafa, niché au cœur des Plaines venteuses, sert de base d'opérations aux chasseurs, offrant des services essentiels et des montures Seikrets pour faciliter leurs expéditions</p>
+        </div>
+    </div>
+    </section>
+
+    <!-- section 2 -->
+
+    <section>
+    <div id="menaces">
+        <div>
+            <h2>Nouvelles menaces</h2>
+        </div>
+        <div>
+            <h3>La wyverne des sables</h3>
+            <p>Le Balahara est un Léviathan introduit dans Monster Hunter Wilds, reconnaissable à son corps serpentin et souple, recouvert de petites écailles brun-grisâtre qui s'élargissent sur les côtés. Il possède quatre pattes courtes et une longue queue en forme de foret. Sa tête distincte présente des mâchoires tripartites avec trois langues, ornées de dents ressemblant à des lames de scie, et une lueur bleue émane de l'intérieur de sa bouche.</p>
+        </div>
+    </div>
+    <button id="anime1">
+            <div>
+                <h4>Lancer l'animation</h4>
+            </div>
+    </button>
+    <div id="balaharaImg"><img src="../img/balahara.png" alt="Balahara"></div>
+    <div id="balahara"><video src="../video/Balahara.mp4" autoplay muted></video></div>
+    </section>
+
+        <!-- section 3 -->
+
+        <section>
+    <div id="end-game">
+        <div>
+            <h2>Contenu End-Game</h2>
+        </div>
+        <div>
+            <h3>Ca vole c'est chiant</h3>
+            <p>L'Hirabami est un Léviathan introduit dans Monster Hunter Wilds, caractérisé par une morphologie unique rappelant celle du Somnacanth. Son corps est principalement de couleur violet foncé avec des nuances de cyan. Sa tête pointue est couverte de plaques segmentées, et sa mâchoire inférieure peut se diviser en deux. L'intérieur de sa bouche est vivement coloré, et il possède deux petits yeux blancs dépourvus de pupilles. Une large membrane s'étend des côtés de sa tête le long de son corps, lui permettant de planer gracieusement dans les airs</p>
+        </div>
+    </div>
+    <button id="anime2">
+            <div>
+                <h4>Lancer l'animation</h4>
+            </div>
+    </button>
+    <div id="hirabamiImg"><img src="../img/hirabami.webp" alt="Hirabami"></div>
+    <div id="hirabami"><video src="../video/Hirabami.mp4" autoplay muted></video></div>
+    </section>
+
+            <!-- section 4 -->
+
+    <section>
+    <div id="biomes">
+        <div>
+            <h2>Biomes etendus et vivants</h2>
+        </div>
+        <div>
+            <h3>Il crache des eclairs, c'est chiant aussi</h3>
+            <p>Son corps est recouvert d'écailles dorées osseuses, avec des écailles noires plus petites sur le ventre, les articulations et les ailes. Contrairement à la plupart des autres wyvernes bipèdes, ses ailes ne sont pas composées de doigts allongés, mais d'une seule membrane semblable à celle des ptérosaures, avec des protubérances tranchantes utilisées comme armes.</p>
+        </div>
+    </div>
+    <button id="anime3">
+            <div>
+                <h4>Lancer l'animation</h4>
+            </div>
+    </button>
+    <div id="RayDauImg"><img src="../img/rayDau.png" alt="RayDau"></div>
+    <div id="RayDau"><video src="../video/RayDau.mp4" autoplay muted></video></div>
+    </section>
+
+</main>
